2e412965b123edc0106338ebe93ca0f60c2adc09
Nelly Sin <nesin@ucsc.edu>
2021-02-02 16:45:24 -0800

    printing the numbers in order

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -8,17 +8,18 @@
 	li $v0, 5		#entering a number, stored in v0
 	syscall
 	
-	# store result in $v0
-	move $t2, $v0
+	# store result in $t1
+	move $t3, $v0
 	
 	#create the i = 1 to start
-	li $t1, 1
-	
+	li $t0, 1
+	li $t1, 0xa
+
 	# create a loop for printing the *astrix and number 
 	num_loop: NOP 
-	bge $t1, $t2, program_exit	# for (i =1, i < user_input, i++)
+	bgt $t0, $t3, program_exit	# for (i =1, i < user_input, i++)
 	li $v0, 1
-	move $a0, $t1
+	move $a0, $t0
 	syscall
 	
 	li $v0, 11
@@ -29,7 +30,9 @@
 	j num_loop
 	
 	program_exit:
+	
 	li $v0, 10
+	syscall
 	
 .data
 	prompt: .asciiz "Enter the height of the pattern (must be greater than 0):"
