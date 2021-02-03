13afe31682a2a03eb011f452d74e3746844272a9
Nelly Sin <nesin@ucsc.edu>
2021-02-02 22:46:11 -0800

    tring to add asterisks

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -1,35 +1,70 @@
+.data
+	prompt: .asciiz "Enter the height of the pattern (must be greater than 0):\t"
+	# Phrase to ask user input
+	
+	asterisk: .asciiz "*"
+	
+	error: .asciiz "Invalid Entry!"
+	
+	space: .asciiz "\t"
+
 .text
-User_input:
-	# Tell user input to ender a number
+user_input:
+	# Tell user to input a number
 	li $v0, 4		#load immediate by address of null-terminated string to
 	la $a0, prompt		#load address in the argument register in prompt
 	syscall 		# system call the user requested
 	
-	entry:
-		# Getting the user input
-		li $v0, 5		#entering a number, stored in v0
-		syscall
-	
-		# store result in $t1
-		move $t3, $v0		#for the num_loop
-	
-		#create the i = 1 to start
-		li $t0, 1		# starting number
-		li $t1, 0xa		# spacing
-	invalid:
-		ble $t3, $zero, program_exit
-		la $a0, error
-
-	# create a loop for printing the *astrix and number
- 
-num_loop: NOP 
-
-	bgt $t0, $t3, program_exit	# for (i =1, i < user_input, i++)
-	li $v0, 1
-	move $a0, $t0
+	# Getting the user input
+	li $v0, 5		#entering a number, stored in v0
 	syscall
 	
-	la $a0, spacing
+	# store result in $t2
+	move $t2, $v0		#for the num_loop
+	
+	#check if t2 is less than or equal to 2
+#new_loop: NOP
+	#blez $t2, exit_newloop
+	#li $v0, 4
+	#la $a0, error
+	#syscall
+	
+	#li $v0, 4		#load immediate by address of null-terminated string to
+	#la $a0, prompt		#load address in the argument register in prompt
+	#syscall 		# system call the user requested
+
+	# Getting the user input
+	#li $v0, 5		#entering a number, stored in v0
+	#syscall
+		
+	#j new_loop
+
+	#exit_newloop:
+	#li $v0, 10
+	#syscall	
+	
+	#create the i = 1 to start
+	li $t0, 1		# starting number
+	li $t1, 0xa		# new line
+	li $t3, 1
+		
+
+	# create a loop for printing the *astrix and number
+num_loop: NOP 
+
+	bgt $t0, $t2, program_exit	# for (i =1, i < user_input, i++)
+	
+	asterisk_loop: NOP 
+		bgt $t1, $t3, add_asterisk
+		
+		add_asterisk:
+		li $v0, 4
+		la $a0, asterisk
+		syscall
+		
+		
+	li $v0, 1
+	move $a0, $t0
 	syscall
 	
 	li $v0, 11
@@ -40,16 +75,6 @@ num_loop: NOP
 	j num_loop
 	
 	program_exit:
-	
 	li $v0, 10
 	syscall
 	
-.data
-	prompt: .asciiz "Enter the height of the pattern (must be greater than 0):"
-	# Phrase to ask user input
-	
-	asterix: .asciiz "*"
-	
-	error: .asciiz "Invalid Entry!"
-	
-	spacing: .asciiz "/t"
