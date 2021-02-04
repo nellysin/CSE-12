c2e144a4c5a72eacf08d8a9f8478dc69d891952a
Nelly Sin <nesin@ucsc.edu>
2021-02-03 18:03:28 -0800

    mac cannot find the older files (new file)

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -10,7 +10,7 @@
 
    main:	# see where loops start and end
 		
-      user_input:	# This is where the user will input the number
+      user_input: NOP	# This is where the user will input the number
          # Tell user to input a number
          li $v0, 4		#load immediate by address of null-terminated string to
 	 la $a0, prompt		#load address in the argument register in prompt
@@ -21,39 +21,54 @@
 	 syscall
 	
 	 # store result in $t2
-	 move $t2, $v0		#for the num_loop
+	 move $t2, $v0		#for first loop as a max number
 		
 	 #check if t2 is greater than 0
-	 bgtz $t2, num_loop 	# if $t2 is <= 0 print error & back to user_input			
+	 bgtz $t2, first_loop 	# if $t2 is <= 0 print error & back to user_input			
 		
 	 #function to print the error message
 	 li $v0, 4		
 	 la $a0, error
 	 syscall
+	 
 	 j user_input
 		
 	
    #create the i = 1 to start
    li $t0, 1		# starting number for num loop
    li $t1, 0xA		# new line
+   
+   
+   # create a loop for printing the number for the pyramid 
+   first_loop: NOP 
+      bgt $t0, $t2, program_exit      # for (i =1, i < user_input, i++) then exit program
 	
-   # create a loop for printing the *astrisk and number for the pyramid 
-   num_loop: NOP 
-
-	 bgt $t0, $t2, program_exit	# for (i =1, i < user_input, i++) then exit program
-		
-	 li $v0, 1		# read integer
-	 move $a0, $t0		# move $t0 to $a0
-	 syscall
-	
-	 li $v0, 11		# read ascii for new line
-	 move $a0, $t1		# move $t1 to $a0
-	 syscall
-	
-	 addi $t0, $t0, 1	# increment t0 (i) by one
-	 j num_loop		# jump back to num_loop
-	
-	 program_exit:		
-	 li $v0, 10		# command for exiting the program
-	 syscall
-	
+      li $v0, 1            # read integer
+      move $a0, $t0      # move $t0 to $a0
+      syscall             #execute the syscall
+      
+      li $v0, 11         # read ASCII
+      la $a0, 0xA         # make a new line
+      syscall            # execute in making a new line
+      
+      addi $t0, $t0, 1	# increment t0 (i) by one
+      
+      li $t3, 1
+      # create a loop for adding the asterisks 
+      second_loop: NOP
+      bgt $t3, $t0, first_loop      #if t3 > t0 go back to first_loop
+      
+      li $v0, 11                     # print a ascii 
+      la $a0, 0x2A                   # print an asterisk
+      syscall                        # system call
+      
+      addi $t3, $t3, 1               # update the t3 counter 
+      j second_loop                  # jump back to the second_loop
+      
+      program_exit:		
+         li $v0, 10		# command for exiting the program
+         syscall
+      
+      
+      
+      
