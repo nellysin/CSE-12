1ade397ce10f3717d73a6bbce0f493f52a51c228
Nelly Sin <nesin@ucsc.edu>
2021-02-04 22:05:39 -0800

    add + fix the second set of '*'

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -8,7 +8,7 @@
 
 .text
 
-   main:	# see where loops start and end
+   main:	# for me to see where loops start and end
 		
       user_input: NOP	# This is where the user will input the number
          # Tell user to input a number
@@ -41,7 +41,7 @@
 	 beq $t0, 1, skip_loop
 	 
          second_loop: NOP
-            bge $t3, $t0, skip_loop      #if t3 >= t0 go back to first_loop
+            bge $t3, $t0, skip_loop      #if t3 >= t0 go to skip loop
 
             li $v0, 11                     # read an ascii 
             la $a0, 0x2A                   # print an asterisk
@@ -54,16 +54,18 @@
             addi $t3, $t3, 1               # update the t3 counter 
             
             j second_loop                  # jump back to the second_loop
-            
+         
 	 skip_loop:
 	    li $t3, 1
 	    li $v0, 1            # read integer
             move $a0, $t0      # move $t0 to $a0
             syscall             #execute the syscall
             
-               beq $t0, 1, skip_loop
-               third_loop: NOP
-               bge $t3, $t0, skip_loop      #if t3 >= t0 go back to first_loop
+            
+            li $t3, 1
+            beq $t0, 1, cont_skip_loop      #if $t0 <= 1 continue to skip loop
+            third_loop: NOP                   # print the second asterisk set
+               bge $t3, $t0, cont_skip_loop      #if t3 >= t0 continue skip_loop
                
                li $v0, 11                    # read an ascii
                la $a0, 0xB                   # print a tab
@@ -74,9 +76,9 @@
                syscall                        # system call
 
                addi $t3, $t3, 1               # update the t3 counter 
-               j third_loop                  # jump back to the second_loop
+               j third_loop                  # jump back to the third_loop
             
-            
+         cont_skip_loop:      # a continuation of skip_loop  
             li $v0, 11         # read ASCII
             la $a0, 0xA         # make a new line
             syscall            # execute in making a new line
@@ -84,6 +86,7 @@
             addi $t0, $t0, 1	# increment t0 (i) by one
             
             bgt $t0, $t2,  program_exit # for (i =1, i < user_input, i++) then exit program 
+	    li $t3, 1
 	    j first_loop 
 	    
       program_exit:		
