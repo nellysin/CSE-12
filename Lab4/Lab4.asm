3574a31df2885e38e2e6dedace6dfbb191dbce12
nesin <nesin@ucsc.edu>
2021-02-15 23:34:38 -0800

    print error message + additional comments

--- Lab4/Lab4.asm
+++ Lab4/Lab4.asm
@@ -26,19 +26,18 @@
       
       move_char: NOP             # loop to check characters
       
-         first_char:
-         lb $a0, ($0)
-         blt $a0, 'A', first_lower
-         ble $a0, 'Z', valid_char
+         first_char:                             # check the first character of the name
+            lb $a0, ($0)                         # load byte to the first character
+            blt $a0, 'A', first_lower            # check if it is less than A try lower case
+            ble $a0, 'Z', valid_char             # check if it is less than Z
          
-         first_lower: NOP
-         blt $a0, 'a', program_exit
-         bgt $a0, 'z', program_exit
+         first_lower:                            # first character to check the lower case
+            blt $a0, 'a', print_error            # if it is less than a then print an error
+            bgt $a0, 'z', print_error            # if it is larger than z then print an error
          
-         valid_char: NOP
-         
-            bgt $t1, 20 program_exit
-            lb $a0, ($t0)
+         valid_char: NOP                         # check if the name is valid
+            bgt $t1, 20 print_error              # if the coutner is greater than 20 print error
+            lb $a0, ($t0)                        # load byte to the first character
             beqz $a0, program_exit
             li $v0, 11
             
@@ -46,7 +45,17 @@
             blt $a0, 'A', loop_lower
             ble $a0, 'Z', next_char
             
+            loop_num: NOP
+            blt $a0, '0', loop_dot
+            ble $a0, '9', next_char
             
+            loop_dot: NOP
+            bne $a0, '.', loop_under
+               j next_char
+            
+            loop_under: NOP
+            bne $a0, '_', program_exit
+               j next_char
       
          next_char:
             addi $t1, $t1, 1                 # move to the next character
@@ -54,6 +63,11 @@
             syscall
             
             j valid_char
+   
+   print_error:
+      li $v0, 4
+      la $a0, error
+      syscall
    
    program_exit:
       li $v0, 10
