105061c324384f8d9c214ef6e44f1bda65838b21
nesin <nesin@ucsc.edu>
2021-02-15 23:16:21 -0800

    check first character

--- Lab4/Lab4.asm
+++ Lab4/Lab4.asm
@@ -16,6 +16,26 @@
       
       li $v0, 4                 # print the file name
       syscall
+      
+      move $t0, a0              # moving $a0 to $t0 as temporary
+      
+      li $v0, 11
+      la $a0, 0xA               # this is for visual
+      
+      li $t1, 0                 # this is the counter for num of characters
+      
+      move_char: NOP             # loop to check characters
+      
+         first_char:
+         lb $a0, ($t0)
+         blt $a0, 'A', first_lower
+         ble $a0, 'Z', valid_char
+         
+         first_lower: NOP
+         blt $a0, 'a', program_exit
+         bgt $a0, 'z', program_exit
+         
+         valid_char: NOP
    
    program_exit:
       li $v0, 10
